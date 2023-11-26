a19f39da0e50755c8f2ef2efddef95c2047de34c
UmamaSayed <umamasayed2910@gmail.com>
2023-11-26 20:24:59 +0530

    Added the Details and VideoTrailersSection of Detail page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import "./style.scss";
 
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from './detailsBanner/DetailsBanner';
+
 const Details = () => {
+  const { mediaType,id } = useParams();
+  const { data, loading } = useFetch (`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch (`/${mediaType}/${id}/videos`);
   return (
-    <div>Details</div>
+    <div>
+    <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+    </div>
   )
 }
 
-export default Details
+export default Details;
